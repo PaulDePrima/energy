9633fb961e9899b06a335fea704f100635dcfb39
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-11-27 09:10:23 -0800

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def render_main():
     return render_template('Home.html')
+    
+@app.route("/facts")
+def render_second():
+    return render_template('facts.html')
+    
+@app.route("/export")
+def render_third():
+    return render_template('exports.html')
+    
+@app.route("/consume")
+def render_four():
+    return render_template('consume.html')
 
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+    app.run(debug=True, port=54321)
