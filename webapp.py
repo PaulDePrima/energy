c49774bfa5840651249574a9ebdb1b4ea0f3e779
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-11-27 09:34:11 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -12,7 +12,13 @@ def render_main():
     
 @app.route("/facts")
 def render_second():
-    return render_template('facts.html')
+    return render_template('facts.html', year = get_year_options())
+
+@app.route("/UsImprt2")
+def render_usprts2():
+       us_imp = request.args["year"]
+       print(us_imp)
+       return render_template('facts.html', year = get_year_options(us_imp))
     
 @app.route("/export")
 def render_third():
@@ -21,6 +27,28 @@ def render_third():
 @app.route("/consume")
 def render_four():
     return render_template('consume.html')
+
+def get_year_options(default = None):
+    options = ""
+    state = ""
+    for c in energy:
+        if not str(c["year"]) == state:
+            if str(c["year"]) == default:
+              options += Markup("<option selected value=\"" + default + "\">" + default + "</option>")
+              print('true')
+            else:
+              options += Markup("<option value=\"" + str(c["year"]) + "\">" + str(c["year"]) + "</option>")
+            state = str(c["year"])
+
+    return options
+
+def us_importEngery(year):
+       imprts = energy[1949-year]["imports"]
+       randKey = random.choice(list(imprts.keys()))
+
+       return randKey + ": " + imprts[randKey] + " Quadrillion BTUs"
+    
+    
 
 if __name__=="__main__":
     app.run(debug=True, port=54321)
