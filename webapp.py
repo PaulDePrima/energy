21e2c5a76e3512604634e70030023498f679ccf2
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-11-30 09:39:25 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,16 +11,20 @@ def render_main():
     return render_template('Home.html')
     
 @app.route("/facts")
-def render_second():
-    return render_template('facts.html', year = get_year_options())
-
-@app.route("/UsImprt2")
 def render_usprts2():
 	try:   
 	    us_imp = request.args["year"]
 	    return render_template('importEnergy.html', year = get_year_options(us_imp), response = us_importEngery(us_imp))
 	except:
 		return render_template('importEnergy.html', year = get_year_options())
+
+#@app.route("/UsImprt2")
+#def render_usprts2():
+#	try:   
+#	    us_imp = request.args["year"]
+#	    return render_template('importEnergy.html', year = get_year_options(us_imp), response = us_importEngery(us_imp))
+#	except:
+#		return render_template('importEnergy.html', year = get_year_options())
     
 @app.route("/export")
 def render_third():
